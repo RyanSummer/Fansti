fa8a373dfe0d2f8671145580c86b51dd59865b1f
chengxianchao <15988161520@139.com>
2020-04-18 19:17:02 +0800

    创建项目：泛思特一期

---
+++ app/src/main/java/com/cxc/fansti/base/MyApplication.java
@@ -0,0 +1,31 @@
+package com.cxc.fansti.base;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+public class MyApplication extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        com.orhanobut.logger.Logger.init("okhttp");
+        /**
+         * 获取Context
+         */
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
